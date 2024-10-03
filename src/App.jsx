e3dd2027e7c28dca9ee50715d49b03bef09391e4
RimzanAhmed <rimzanfaiz701@gmail.com>
2024-10-02 17:24:31 -0700

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ function App() {
         <h1>Hello World!</h1>
         <p>This is my first pull request testing!</p>
         <p>This is second testing !</p>
+         
+        <h1>Hello World!</h1>
+        <p>This is my first pull request testing!</p>
+        <p>This is second testing !</p>
+
+         <h1>Hello World!</h1>
+        <p>This is my first pull request testing!</p>
+        <p>This is second testing !</p>
+        
 
         <p>This is the new testing pull request from the newly created branch</p>
         <h2>This is new testing after the new changes of the branch rules</h2>
